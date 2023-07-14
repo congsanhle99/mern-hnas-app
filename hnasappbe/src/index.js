85e18317475f3a7c7e582a6c6d1cf198b0f89184
CongSanhLe <congsanhle1999@gmail.com>
2023-07-15 05:42:47 +0700

    [0206] logger (instead console.log) with winston

--- hnasappbe/src/index.js
+++ hnasappbe/src/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
+import logger from "./configs/logger.configs.js";
 
 // env variable
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}...`);
+  logger.info(`Server is listening at ${PORT}...`);
 });
