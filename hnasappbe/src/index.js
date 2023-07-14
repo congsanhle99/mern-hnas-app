f350bfed7b66e67658c455f225d8655d462cdeee
CongSanhLe <congsanhle1999@gmail.com>
2023-07-15 05:14:03 +0700

    [0202] setup basic express app

--- hnasappbe/src/index.js
+++ hnasappbe/src/index.js
@@ -1,1 +1,12 @@
-console.log("hihihihihihihihi!");
+import dotenv from "dotenv";
+import app from "./app.js";
+
+// dotenv config
+dotenv.config();
+
+// env variable
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}...`);
+});
