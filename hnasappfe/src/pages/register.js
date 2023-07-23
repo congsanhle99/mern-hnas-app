260e880bcb3cfcf973184a87489b23da7ca2dde0
CongSanhLe <congsanhle1999@gmail.com>
2023-07-23 07:04:29 +0700

    [0501] Register and login page

--- hnasappfe/src/pages/register.js
+++ hnasappfe/src/pages/register.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import RegisterForm from "../components/auth/RegisterForm";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[18px] overflow-hidden">
+      {/* Container  */}
+      <div className="flex w-[1600px] mx-auto h-full">
+        {/* Register form */}
+        <RegisterForm />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
